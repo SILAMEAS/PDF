c232be06a8bb16fdd84b1a50411a4d87c91b8931
SILAMEAS <silam8422@gmail.com>
2022-12-21 10:37:37 +0700

    Config_Platform

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import React from "react";
-import Rsq from "./components/RPS";
-import ReadPDF from "./components/Read";
+import ReadPDF from "./src/mobile/Read";
+import ReadPDF_w from "./src/mobile/Read";
+// import Rsq from "./src/mobile/RPS";
 
 export default function App() {
+  const isWeb = Platform.OS === "web";
   return (
-    <View style={styles.container}>
-      <Text>
-        <ReadPDF />
-      </Text>
-    </View>
+    <View style={styles.container}>{isWeb ? <ReadPDF_w /> : <ReadPDF />}</View>
   );
 }
 
